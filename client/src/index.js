00766c3ea862cf0299f3f1f4986d942242590796
Jeremiah <jsnidar@gmail.com>
2021-12-15 09:56:16 -0600

    Add dependencies react-bootstrap, bootstrap, react-dom-router

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
