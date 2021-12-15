20ce7410a9eb11a65017032d25cd54fd476695be
Jeremiah <jsnidar@gmail.com>
2021-12-15 07:51:15 -0600

    Add validates_timeliness gem to validate dates in models

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -1,3 +1,6 @@
 class CheckIn < ApplicationRecord
+  validates :weight, presence: true, numericality: { greater_than: 0 }
+  validates_date :date, on_or_before: lambda { Date.current }
+
   belongs_to :user
 end
